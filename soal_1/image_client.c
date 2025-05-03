a8e2286835180951d6943cc028a8037a6eed0952
103_NI'MAH FAUZIYYAH ATOK <nmhfauziyyah@gmail.com>
2025-05-03 23:38:15 +0700

    nomor1

--- soal_1/image_client.c
+++ soal_1/image_client.c
@@ -1,1 +1,167 @@
+// File: image_client.c
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+#define PORT 8080
+#define SERVER_ADDR "127.0.0.1"
+#define BUFFER_SIZE 8192
+
+// Cek apakah folder ada
+int folder_exists(const char* path) {
+    struct stat st;
+    return stat(path, &st) == 0 && S_ISDIR(st.st_mode);
+}
+
+// Jalankan perintah shell
+void run_command(const char* cmd) {
+    system(cmd);
+}
+
+// Download dan unzip secrets
+void prepare_secrets() {
+    if (!folder_exists("client/secrets")) {
+        printf("[Client]: Downloading secrets from Google Drive...\n");
+        run_command("mkdir -p client");
+        run_command("cd client && wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=15mnXpYUimVP1F5Df7qd_Ahbjor3o1cVw' -O secrets.zip");
+        run_command("cd client && unzip -o secrets.zip && rm secrets.zip");
+    }
+}
+
+// Kirim ke server dan terima balasan
+int communicate_with_server(const char* message, unsigned char* response, int* response_size) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        printf("[Client]: ERROR creating socket.\n");
+        return -1;
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        printf("[Client]: ERROR connecting to server.\n");
+        close(sock);
+        return -1;
+    }
+
+    send(sock, message, strlen(message), 0);
+    *response_size = recv(sock, response, BUFFER_SIZE, 0);
+    close(sock);
+    return 0;
+}
+
+// Balikkan string
+void reverse_string(char* str) {
+    int len = strlen(str);
+    for (int i = 0; i < len/2; i++) {
+        char tmp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = tmp;
+    }
+}
+
+int main() {
+    prepare_secrets();
+
+    while (1) {
+        printf("========================\n");
+        printf("| Image Decoder Client |\n");
+        printf("========================\n");
+        printf("1. Send input file to server\n");
+        printf("2. Download file from server\n");
+        printf("3. Exit\n>> ");
+
+        int choice;
+        scanf("%d", &choice);
+        getchar(); // flush newline
+
+        if (choice == 1) {
+            char filename[128];
+            printf("Enter the file name: ");
+            fgets(filename, sizeof(filename), stdin);
+            filename[strcspn(filename, "\n")] = '\0';
+
+            char filepath[256];
+            snprintf(filepath, sizeof(filepath), "client/secrets/%s", filename);
+
+            FILE* file = fopen(filepath, "r");
+            if (!file) {
+                printf("[Client]: ERROR file not found.\n");
+                continue;
+            }
+
+            char* hexdata = malloc(BUFFER_SIZE);
+            size_t read = fread(hexdata, 1, BUFFER_SIZE - 1, file);
+            fclose(file);
+            hexdata[read] = '\0';
+
+            reverse_string(hexdata);
+
+            char* request = malloc(strlen(hexdata) + 20);
+            sprintf(request, "DECRYPT:%s", hexdata);
+
+            unsigned char response[BUFFER_SIZE];
+            int resp_size = 0;
+
+            if (communicate_with_server(request, response, &resp_size) == 0) {
+                response[resp_size] = '\0';
+                printf("Server: Text decrypted and saved as %s\n", response);
+            }
+
+            free(hexdata);
+            free(request);
+
+        } else if (choice == 2) {
+            char filename[128];
+            printf("Enter the file name: ");
+            fgets(filename, sizeof(filename), stdin);
+            filename[strcspn(filename, "\n")] = '\0';
+
+            char request[160];
+            snprintf(request, sizeof(request), "DOWNLOAD:%s", filename);
+
+            unsigned char response[BUFFER_SIZE];
+            int resp_size = 0;
+
+            if (communicate_with_server(request, response, &resp_size) == 0) {
+                if (resp_size < 24 && strncmp((char*)response, "ERROR", 5) == 0) {
+                    printf("[Client]: ERROR server response: %s\n", response);
+                } else {
+                    char save_path[256];
+                    snprintf(save_path, sizeof(save_path), "client/%s", filename);
+
+                    FILE* out = fopen(save_path, "wb");
+                    if (out) {
+                        fwrite(response, 1, resp_size, out);
+                        fclose(out);
+                        printf("Success! Image saved as %s\n", filename);
+                    } else {
+                        printf("[Client]: ERROR saving file.\n");
+                    }
+                }
+            }
+
+        } else if (choice == 3) {
+            time_t now = time(NULL);
+            struct tm* t = localtime(&now);
+            printf("[Client][%04d-%02d-%02d %02d:%02d:%02d]: [EXIT] [Client requested to exit]\n",
+                t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
+                t->tm_hour, t->tm_min, t->tm_sec);
+            break;
+        } else {
+            printf("[Client]: Invalid choice.\n");
+        }
+    }
+
+    return 0;
+}
--- soal_1/image_server.c
+++ soal_1/image_server.c
@@ -1,1 +1,164 @@
+// File: image_server.c
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 8192
+#define LOG_PATH "server/server.log"
+#define DB_PATH "server/database/"
+
+// Logging helper
+void write_log(const char* source, const char* action, const char* info) {
+    FILE* log = fopen(LOG_PATH, "a");
+    if (!log) return;
+    time_t now = time(NULL);
+    struct tm* t = localtime(&now);
+    fprintf(log, "[%s][%04d-%02d-%02d %02d:%02d:%02d]: [%s] [%s]\n",
+        source, t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
+        t->tm_hour, t->tm_min, t->tm_sec, action, info);
+    fclose(log);
+}
+
+// Hex decode
+int hex_to_bin(const char* hex, unsigned char* out) {
+    int len = strlen(hex);
+    for (int i = 0; i < len; i += 2) {
+        if (sscanf(hex + i, "%2hhx", &out[i / 2]) != 1) return -1;
+    }
+    return len / 2;
+}
+
+// Reverse string in place
+void reverse_string(char* str) {
+    int len = strlen(str);
+    for (int i = 0; i < len/2; i++) {
+        char tmp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = tmp;
+    }
+}
+
+// Daemonize
+void daemonize() {
+    pid_t pid = fork();
+    if (pid < 0) exit(EXIT_FAILURE);
+    if (pid > 0) exit(EXIT_SUCCESS); // Parent exits
+    umask(0);
+    setsid();
+    chdir("/");
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
+
+// Ensure directories exist
+void ensure_directories() {
+    mkdir("server", 0755);
+    mkdir(DB_PATH, 0755);
+}
+
+int main() {
+    ensure_directories();
+    daemonize();
+
+    int server_fd, client_fd;
+    struct sockaddr_in addr;
+    char buffer[BUFFER_SIZE];
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) exit(EXIT_FAILURE);
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) exit(EXIT_FAILURE);
+    if (listen(server_fd, 5) < 0) exit(EXIT_FAILURE);
+
+    while (1) {
+        client_fd = accept(server_fd, NULL, NULL);
+        if (client_fd < 0) continue;
+
+        memset(buffer, 0, BUFFER_SIZE);
+        recv(client_fd, buffer, BUFFER_SIZE, 0);
+
+        if (strncmp(buffer, "DECRYPT:", 8) == 0) {
+            write_log("Client", "DECRYPT", "Text data");
+
+            char* hexdata = buffer + 8;
+            reverse_string(hexdata);
+
+            int hex_len = strlen(hexdata);
+            unsigned char* bindata = malloc(hex_len / 2);
+            int bin_len = hex_to_bin(hexdata, bindata);
+
+            if (bin_len < 0) {
+                send(client_fd, "ERROR: Failed to decode hex.\n", 30, 0);
+                free(bindata);
+                close(client_fd);
+                continue;
+            }
+
+            time_t now = time(NULL);
+            char filename[64];
+            snprintf(filename, sizeof(filename), "%ld.jpeg", now);
+
+            char fullpath[128];
+            snprintf(fullpath, sizeof(fullpath), "%s%s", DB_PATH, filename);
+
+            FILE* img = fopen(fullpath, "wb");
+            if (!img) {
+                send(client_fd, "ERROR: Failed to save image.\n", 30, 0);
+                free(bindata);
+                close(client_fd);
+                continue;
+            }
+
+            fwrite(bindata, 1, bin_len, img);
+            fclose(img);
+
+            write_log("Server", "SAVE", filename);
+            send(client_fd, filename, strlen(filename), 0);
+            free(bindata);
+
+        } else if (strncmp(buffer, "DOWNLOAD:", 9) == 0) {
+            char* filename = buffer + 9;
+            write_log("Client", "DOWNLOAD", filename);
+
+            char fullpath[128];
+            snprintf(fullpath, sizeof(fullpath), "%s%s", DB_PATH, filename);
+            FILE* img = fopen(fullpath, "rb");
+            if (!img) {
+                send(client_fd, "ERROR: File not found.\n", 24, 0);
+                write_log("Server", "ERROR", "File not found");
+                close(client_fd);
+                continue;
+            }
+
+            fseek(img, 0, SEEK_END);
+            long size = ftell(img);
+            rewind(img);
+
+            unsigned char* data = malloc(size);
+            fread(data, 1, size, img);
+            fclose(img);
+
+            write_log("Server", "UPLOAD", filename);
+            send(client_fd, data, size, 0);
+            free(data);
+        }
+
+        close(client_fd);
+    }
+
+    return 0;
+}
